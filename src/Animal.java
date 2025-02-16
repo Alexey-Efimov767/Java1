d44ef5299baf78b8c68fb923562bc6027f5663ac
Alexey Efimov <alexefimov565@gmail.com>
2025-02-16 20:33:02 +0300

    This is example of java classes

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+public abstract class Animal {
 
-public class Animal {
+    public final static int QUANTITYofEARS = 2;
+
+    private int weight;
+    private String name;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    private void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    void voice(){
+        System.out.println("The voice of animal");
+    }
+
+    public Animal(int weight, String name) {
+        this.weight = weight;
+        this.name = name;
+    }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class Dog extends Animal {
+    public Dog(int weight, String name) {
+        super(weight, name);
+    }
 
-public class Dog {
+    @Override
+    void voice() {
+        System.out.println("Barking");
+    }
+
+    int eatenFood(int a, int b){
+        int x = a+b;
+        System.out.println("Dog ate " + x + " kg");
+        return x;
+    }
+    double eatenFood(double a, double b){
+        System.out.println("Dog ate "+a+b+ " kg");
+        return a+b;
+    }
 }
